e13ce61fec0ae9e21222445a91ff4a829554eb00
GITTOGEORGETHOMAS <gittothomas8@gmail.com>
2025-07-17 23:41:53 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Gitto", lastName: "Thomas" });
-});
+const userHandlers = [
+  (req, res, next) => {
+    console.log("Handling the route user1!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user2!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user3!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user4!!");
+    res.send("4th Response back");
+  }
+]
 
-app.post("/user", (req,res) => {
-    res.send("Data successfully saved to the database!");
-});
+app.use("/user", userHandlers);
 
-app.delete("/user", (req,res) => {
-    res.send("Deleted Successfully");
-})
 
 app.listen(7777, () => {
   console.log("Server is running on port 7777...");
