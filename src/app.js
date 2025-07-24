4b11c6507d0eb041fcbf61624d91d1ece8f4c465
GITTOGEORGETHOMAS <gittothomas8@gmail.com>
2025-07-24 16:08:16 +0530

    multiple route handlres more detialed

--- src/app.js
+++ src/app.js
@@ -1,26 +1,72 @@
 const express = require("express");
 const app = express();
 
-const userHandlers = [
-  (req, res, next) => {
-    console.log("Handling the route user1!!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user2!!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user3!!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user4!!");
-    res.send("4th Response back");
-  }
-]
+// app.use("/user", 
+//   (req,res,next) => {
+//    console.log("Handler 1");
+//    next();
+// },
+//   (req,res,next) => {
+//    console.log("Handler 2");
+//    next();
+// },
+//   (req,res,next) => {
+//    console.log("Handler 3");
+//    next(); 
+// },
+//   (req,res) => {
+//    console.log("Handler 4");
+//    res.send("FInal Response from Handler 4");
+// },
 
-app.use("/user", userHandlers);
+// );
+
+// const handler1 = (req, res, next) => {
+//   console.log("Handler 1");
+//   next();
+// };
+
+// const handler2 = (req, res, next) => {
+//   console.log("Handler 2");
+//   next();
+// };
+
+// const handler3 = (req, res, next) => {
+//   console.log("Handler 3");
+//   next();
+// };
+
+// const handler4 = (req, res) => {
+//   console.log("Handler 4");
+//   res.send("Final Response from Handler 4");
+// };
+
+// app.use("/user", [handler1, handler2, handler3, handler4]);
+
+// // Get /user
+// app.get("/user", (req, res) => {
+
+//     console.log("Route hit, but no response sent");
+//     res.send("GET request to /user");
+// });
+
+// //Post /user
+// app.post("/user", (req,res) => {
+//   res.send("POST request to /user");
+// });
+
+// // PUT /user
+// app.put("/user", (req,res) => {
+//   res.send("PUT request to /user");
+// });
+
+// // DELETE /user
+// app.delete("/user", (req, res) => {
+//   res.send("DELETE request to /user");
+// });
+
+
+
 
 
 app.listen(7777, () => {
